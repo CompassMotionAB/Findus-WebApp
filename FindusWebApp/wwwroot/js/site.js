cdd34793cd35ab3eee3c76e7b565349c1f877112
Joaqim Planstedt <git@joaqim.xyz>
2022-02-18 12:19:04 +0100

    Front-End additions and fixes

--- FindusWebApp/wwwroot/js/site.js
+++ FindusWebApp/wwwroot/js/site.js
@@ -18,8 +18,46 @@ function getQueryParameters(str) {
       }.bind({})
     )[0];
 }
+function updateUrlParam(url, param, value) {
 
+    var index = url.indexOf("?");
+
+    if (index > 0) {
+
+        var u = url.substring(index + 1).split("&");
+
+        var params = new Array(u.length);
+
+        var p;
+        var found = false;
+
+        for (var i = 0; i < u.length; i++) {
+            params[i] = u[i].split("=");
+            if (params[i][0] === param) {
+                params[i][1] = value;
+                found = true;
+            }
+        }
+
+        if (!found) {
+            params.push(new Array(2));
+            params[params.length - 1][0] = param;
+            params[params.length - 1][1] = value;
+        }
+
+        var res = url.substring(0, index + 1) + params[0][0] + "=" + params[0][1];
+        for (var i = 1; i < params.length; i++) {
+            res += "&" + params[i][0] + "=" + params[i][1];
+        }
+        return res;
+
+    } else {
+        return url + "?" + param + "=" + value;
+    }
+
+}
 function replaceQueryParam(param, newval, url) {
+ 
   var regex = new RegExp("([?;&])" + param + "[^&;]*[;&]?");
   var query = url.replace(regex, "$1").replace(/&$/, "");
   
