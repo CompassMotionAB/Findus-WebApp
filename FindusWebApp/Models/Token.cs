3a3c36a6f90e1b2edff7d644bf13a466868b6fb8
Joaqim Planstedt <git@joaqim.xyz>
2022-03-03 09:49:26 +0100

    Revert "Merge pull request #8 from CompassMotionAB/joaqim-development-1"
    This reverts commit 83e35db91de1955d95d84858626b90ea2bf8b454, reversing
    changes made to 2749df6e11d1e85eb3a623c458f162b3671681a7.

--- FindusWebApp/Models/Token.cs
+++ FindusWebApp/Models/Token.cs
@@ -1,14 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-
-namespace FindusWebApp.Models
+﻿namespace FindusWebApp.Models
 {
     public class Token
     {
-        [Column(TypeName = "VARCHAR")]
-        [StringLength(50)]
-        [Key]
         public string RealmId { get; set; }
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
